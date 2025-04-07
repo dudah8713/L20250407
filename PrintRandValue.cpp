19e529e7bf1f64017939a7b14f19f4acf72f7f11
Zer0_0m <52215019+dudah8713@users.noreply.github.com>
2025-04-07 09:45:13 +0900

    Print Rand Value
    Print Rand Value

--- PrintRandValue.cpp
+++ PrintRandValue.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <Windows.h>
+#include <unordered_map>
 
 using namespace std;
 
 int main()
 {
-	std::srand(GetTickCount64());
-	int randValue = std::rand() % 53;
+	std::unordered_map<int, bool> unmapBall;
+	bool isAllPrint = true;
+	int nunmber = 0;
 
-	cout << randValue;
+	for (; nunmber < 53; nunmber++)
+	{
+		unmapBall.insert(make_pair(nunmber, false));
+	}
+
+	std::srand(GetTickCount64());
 	
+	while(isAllPrint)
+	{
+		int randValue = std::rand() % 53;
+
+		auto iter = unmapBall.find(randValue);
+		
+		if (iter->second == false)
+		{
+			cout << iter->first << endl;
+			iter->second = true;
+			nunmber--;
+		}
+
+		if (nunmber == 0)
+			isAllPrint = false;
+	}
+
 	return 0;
 }
