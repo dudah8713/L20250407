5366504979b20eeab9ccabbb1f519921da3952f9
Zer0_0m <52215019+dudah8713@users.noreply.github.com>
2025-04-08 09:14:10 +0900

    1등 당첨 기원합니다
    2등 당첨 기원합니다

---
+++ Lotto.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int ArrLotto[45] = { 0, };
+
+void Initialize()
+{
+    for (int Number = 0; Number < 45; Number++)
+    {
+        ArrLotto[Number] = Number + 1;
+    }
+}
+
+void Shuffle()
+{
+    srand(time(NULL));
+    random_shuffle(std::begin(ArrLotto), std::end(ArrLotto));
+}
+
+void Print()
+{
+    for (int Number = 0; Number < 6; Number++)
+    {
+        cout << ArrLotto[Number] << endl;
+    }
+}
+
+int main()
+{
+    Initialize();
+    Shuffle();
+    Print();
+    
+    return 0;
+}
